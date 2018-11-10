bdc7c7f0eb3b72e4a16aed58ce32d434b44fb979
jiimooyou <296738692@qq.com>
2018-11-10 10:59:29 +0800

    Default

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,24 +10,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function upload()
-    {
-        $file = request()->file('file');
-        $url = Qiniu::upload($file);
-        if($url) {
-            return response()->json(['status' => 'success', 'msg' => '上传成功', 'url' => $url]);
-        }
-
-        return response()->json(['status' => 'error', 'msg' => '上传失败']);
-    }
-
-    public function delete()
-    {
-        if(Qiniu::delete(request('url'))) {
-            return response()->json(['status' => 'success', 'msg' => '删除成功']);
-        }
-
-        return response()->json(['status' => 'error', 'msg' => '删除失败']);
-    }
 }
