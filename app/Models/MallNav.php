6a3e6e6f0c6934cd08c56e3c22232e340d1f181f
jimocc <245629490@qq.com>
2018-11-12 15:01:57 +0800

    composer

---
+++ app/Models/MallNav.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class MallNav extends Model
+{
+    //
+    protected $table = 'mall_navs';
+    protected $guarded=[];
+}
