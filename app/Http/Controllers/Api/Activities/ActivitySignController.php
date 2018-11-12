5716214fae5ae0e4c06363f8266776003660cd12
jiimooyou <296738692@qq.com>
2018-11-12 17:49:52 +0800

    hao

--- app/Http/Controllers/Api/Activities/ActivitySignController.php
+++ app/Http/Controllers/Api/Activities/ActivitySignController.php
@@ -9,6 +9,8 @@
 namespace App\Http\Controllers\Api\Activities;
 
 use App\Http\Controllers\Controller;
+use App\Models\Activity;
+
 class ActivitySignController extends Controller
 {
      public function show()
@@ -18,7 +20,18 @@ class ActivitySignController extends Controller
 
      public function signIn()
      {
-
+        $fan_id='';
+        $activity=Activity::find(request()->avtivity_id);
+        $activity_sign_count=$activity->withCount();
+        if($activity_sign_count>=$activity->places){
+            return response()->json(['status' => 'error', 'msg' => '报名人数已满']);
+        }
+        if($activity->sign_type==1){
+            //微信支付
+        }
+        $sign_in=$activity->fan()
+        ->attach($fan_id,['name'=>request()->name,'contact_way'=>request()->contact_way]);
+         return response()->json(['status' => 'success', 'msg' => $sign_in]);
      }
 
 
