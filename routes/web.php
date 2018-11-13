01fcd89f21dd7cb0240833449dcf64fc63b20b5a
dongfang <1163129165@qq.com>
2018-11-13 16:07:51 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -33,11 +33,3 @@ Route::group(['middleware' => ['cors']], function () {
 
 
 });
-Route::apiResource('mallnavs', 'Api\Malls\MallNavController');
-Route::apiResource('mallgoods', 'Api\Malls\MallGoodController');
-Route::post('mallgoods/{mallgood}','Api\Malls\MallGoodController@show')->name('mallgoods');
-Route::apiResource('mallswipers', 'Api\Malls\MallSwiperController');
-Route::apiResource('mallgroups', 'Api\Malls\MallSwiperGroupController');
-Route::apiResource('activitys','Api\Activities\ActivityController');
-
-Route::post('activitys/{activity}','Api\Activities\ActivityController@show')->name('activitys');
