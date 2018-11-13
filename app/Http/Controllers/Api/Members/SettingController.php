a99018158152268fddd925500dae9aa6637370d2
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-13 15:34:58 +0800

    commit

---
+++ app/Http/Controllers/Api/Members/SettingController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api\Members;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SettingController extends Controller
+{
+    public function index() 
+    {
+        $setting = Setting::first();
+        $setting['offer'] = json_decode($setting['offer']);        
+        $groups = Group::getGroup();
+        return response()->json(['status' => 'success', 'setting' => $setting, 'groups' => $groups]);   
+    }
+
+    public function store(SettingRequest $request) 
+    {   
+        $data = request()->all();  
+
+        //满减
+        // [
+        //     ['group_id' => 1, 'condition'=> 100, 'discount'=>  10],
+        //     ['group_id' =>  2, 'condition'=> 100, 'discount'=> 20]
+        // ];
+
+        //折扣
+        // [
+        //     ['group_id '=> 1, 'discount' => 9.5],
+        //     ['group_id'=> 2, 'discount'=>  9]
+        // ];
+
+        $data['offer'] = json_encode($data['offer']);
+
+        if(Setting::create($data)) {
+            return response()->json(['status' => 'success', 'msg' => '新增成功！']);                             
+        }
+
+        return response()->json(['status' => 'error', 'msg' => '新增失败！']);                           
+        
+    }
+
+    public function show()
+    {
+        $setting = Setting::find(request()->setting);
+        $offer = json_decode($setting->offer,true);   
+        $status = $setting ? 'success' : 'error';
+        return response()->json(['status' => $status, 'data' => $setting]);   
+    }
+
+    public function update(SettingRequest $request)
+    {
+        $data = request()->all();   
+
+        $data['offer'] = json_encode($data['offer']);
+             
+        if(Setting::where('id', request()->setting)->update($data)) {
+            return response()->json(['status' => 'success', 'msg' => '更新成功！']);                             
+        }
+
+        return response()->json(['status' => 'error', 'msg' => '更新失败！']);                            
+    }
+
+    public function destroy()
+    {
+        if(Setting::where('id', request()->setting)->delete()) {
+            return response()->json(['status' => 'success', 'msg' => '删除成功！']);                              
+        }
+
+        return response()->json(['status' => 'error', 'msg' => '删除失败！']);     
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,17 @@ Route::group(['middleware' => ['cors']], function () {
     Route::apiResource('mallnavs', 'Api\Malls\MallNavController');
     Route::apiResource('mallgoods', 'Api\Malls\MallGoodController');
     Route::apiResource('activitys','Api\Activities\ActivityController');
+    //会员卡
+    Route::post('/members/changeIntegral', 'Api\Members\MemberController@changeIntegral');
+    Route::post('/members/join', 'Api\Members\MemberController@join');
+    Route::get('/members/{member_id}/selectTag', 'Api\Members\MemberController@selectTag');
+    Route::post('/members/addTag', 'Api\Members\MemberController@addTag');
+    Route::post('/members/deleteTag', 'Api\Members\MemberController@deleteTag');
+    Route::apiResource('/members', 'Api\Members\MemberController');
+    //会员卡设置
+    Route::apiResource('member_settings', 'Api\Members\SettingController');
+    //会员标签
+    Route::apiResource('member_tags', 'Api\Members\TagController');
+
 
 });
