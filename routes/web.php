b2abcdafbda1ecc29cea5755f7c28d265f200fab
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-14 16:18:34 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,6 @@ Route::group(['middleware' => ['cors']], function () {
 Route::get('authorize', function() {
     $app = \EasyWeChat\Factory::officialAccount(config('wechat.official_account.default'));
     $oauth = $app->oauth;
-    dd($oauth);
     // 未登录
     if (empty($_SESSION['wechat_user'])) {
 
