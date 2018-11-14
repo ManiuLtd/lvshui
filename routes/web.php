05776dcd6183cdd4132c25afe0673b7fee407afc
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-14 21:32:41 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -57,8 +57,22 @@ Route::get('authorize', function() {
     $app = \EasyWeChat\Factory::officialAccount(config('wechat.official_account.default'));
 
     $oauth = $app->oauth;
-    dd(session('wechat_user'));
+    // dd(session('wechat_user'));
+    // 未登录
+    if (empty(session('wechat_user'))) {
+        
+
+        session(['target_url' => 'wechat']);
     
+        return $oauth->redirect();
+        // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
+        // $oauth->redirect()->send();
+    }
+    
+    // 已经登录过
+    $user = session('wechat_user');
+
+    return $user;
 });
 
 
@@ -68,8 +82,8 @@ Route::get('oauth_callback', function() {
     // 获取 OAuth 授权结果用户信息
     $user = $oauth->user();
 
-    session(['wechat_user'=> $user->toArray()]);
-    
+    session(['wechat_user' => $user->toArray()]);
+    dd(session('wechat_user'));
     $targetUrl = empty(session('target_url')) ? '/' : session('target_url');
 
     return redirect($targetUrl);
