63fa0278c2336b363811ba1de67f76be0b00f648
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-14 23:07:40 +0800

    commit

--- app/Http/Controllers/Api/Fans/FanController.php
+++ app/Http/Controllers/Api/Fans/FanController.php
@@ -21,7 +21,6 @@ class FanController extends Controller
         // 获取 OAuth 授权结果用户信息
         $user = $oauth->user()->getOriginal();
         $user['privilege'] = json_encode($user['privilege']);
-        dd($user);
         $token = \App\Services\officialAccountToken::getToken($user);
         return response()->json(['token' => $token]);
     }
