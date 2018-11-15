797fbf949f9848cd85e649482d8e26fb67860119
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-15 15:24:51 +0800

    commit

--- app/Utils/Common.php
+++ app/Utils/Common.php
@@ -5,7 +5,7 @@ namespace App\Utils;
 
 class Common 
 {
-    public function generateOrderNo() 
+    public static function generateOrderNo() 
     {
         return date('Ymd').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
     }
