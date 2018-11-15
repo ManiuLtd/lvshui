3ef8173c209b4c4f045eb43f0624893d0132ee9a
jiimooyou <296738692@qq.com>
2018-11-15 15:53:44 +0800

    hao

---
+++ app/Http/Controllers/Api/Activities/ClubController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 29673
+ * Date: 2018/11/15
+ * Time: 15:46
+ */
+
+namespace App\Http\Controllers\Api\Activities;
+
+
+use App\Http\Controllers\Controller;
+
+class ClubController extends Controller
+{
+
+}
