62ccd9acded676078ed1b6995d5eb938f1082779
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-15 15:24:13 +0800

    commit

--- app/Http/Controllers/Api/Wechat/PayController.php
+++ app/Http/Controllers/Api/Wechat/PayController.php
@@ -10,7 +10,7 @@ class PayController extends Controller
 {
     public function pay()
     {
-        $pay = WechatPay::getApp();
+        
         $order = [
             'body' => '任意门支付测试',
             'out_trade_no' => \App\Utils\Common::generateOrderNo(),
@@ -18,8 +18,9 @@ class PayController extends Controller
             'trade_type' => 'JSAPI',
             'openid' => 'oLOcY0jf0SLhG_LN27yU0FIZJWUo',
         ];
-
-        $payment = $pay->pay($order);
+        
+        $payment = WechatPay::pay($order);
+        
 
         dd($payment);
     }
