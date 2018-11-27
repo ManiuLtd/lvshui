04fc95bfe0a49b61eab32cae8a6af8bf6561a5ba
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-27 15:34:44 +0800

    commit

--- app/Http/Controllers/Api/Fans/FanController.php
+++ app/Http/Controllers/Api/Fans/FanController.php
@@ -29,7 +29,7 @@ class FanController extends Controller
 
         $token = $officialAccountToken->getToken($user);
 
-        return redirect(session('url').'?token='.$token);
+        return header('location:'.session('url').'?token='.$token);
     }
 
     public function verifyToken() 
