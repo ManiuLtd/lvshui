c93a588f12495206d1874529ebe510d8d6cacf9e
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-28 17:10:15 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::group(['middleware' => ['cors', 'token']], function () {
 
     Route::get('get-uid','Api\Fans\FanController@getUid');
     Route::get('user','Api\Fans\FanController@getUser');
-    Route::get('config','Api\Fans\FanController@getBasicConfig');
+    Route::get('config','Api\Fans\FanController@getConfig');
     Route::post('wechat/verify', 'Api\Fans\FanController@verifyToken'); //验证Token
 
     Route::group(['prefix' => 'member'], function () {
