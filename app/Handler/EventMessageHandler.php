ac1fb64458eda24a58bf19c432f4f2b2ca8fc92f
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-12-02 13:50:57 +0800

    commit

---
+++ app/Handler/EventMessageHandler.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\Handler;
+
+use EasyWeChat\Kernel\Contracts\EventHandlerInterface;
+
+class EventMessageHandler implements EventHandlerInterface
+{
+    public function handle($payload) 
+    {
+        Log::info($payload);
+    }
+}
