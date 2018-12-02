6b7b5e21dd6548baac86d6d5c8625c336531ca86
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-12-02 17:30:07 +0800

    commit

--- app/Http/Controllers/Api/Wechat/OfficialAccountController.php
+++ app/Http/Controllers/Api/Wechat/OfficialAccountController.php
@@ -19,42 +19,8 @@ class OfficialAccountController extends Controller
         \Log::info('傻逼微信');
         $app = Factory::officialAccount(config('wechat.official_account.default'));
 
-        // $app->server->push(TextMessageHandler::class, Message::TEXT);
-        // $app->server->push(EventMessageHandler::class, Message::EVENT);
-        $app->server->push(function ($message) {
-            switch ($message['MsgType']) {
-                case 'event':
-                    return '收到事件消息';
-                    break;
-                case 'text':
-                    return '收到文字消息';
-                    break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                case 'file':
-                    return '收到文件消息';
-                // ... 其它消息
-                default:
-                    return '收到其它消息';
-                    break;
-            }
-        
-            // ...
-        });
-    
+        $app->server->push(TextMessageHandler::class, Message::TEXT);
+        $app->server->push(EventMessageHandler::class, Message::EVENT);
         $response = $app->server->serve();
     
         return $response;
