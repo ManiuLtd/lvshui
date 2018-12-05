1d8d9c29e90ad9101af61c7f06614f585697491a
jimocc <245629490@qq.com>
2018-12-05 11:09:27 +0800

    composer

---
+++ app/Models/MallSetting.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MallSetting extends Model
+{
+    protected $table = 'mall_settings';
+    protected $guarded=[];
+}
