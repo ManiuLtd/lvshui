200d51fc1f64fbaafc27759bbfab38927ae6c043
jimocc <245629490@qq.com>
2018-12-14 11:06:06 +0800

    composer

--- app/Models/MallGoodGroup.php
+++ app/Models/MallGoodGroup.php
@@ -8,4 +8,17 @@ class MallGoodGroup extends Model
 {
     protected $table = 'mall_good_groups';
     protected $guarded=[];
+
+    public function childGroups()
+    {
+        return $this->hasMany(MallGoodGroup::class,'head_id','id');
+    }
+
+    public function allChildrenGroups(){
+        return $this->childGroups()->with('allChildrenGroups');
+    }
+    public function order()
+    {
+        return $this->hasOne(Order::class,'id','order_id');
+    }
 }
