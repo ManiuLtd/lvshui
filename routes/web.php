0aa8d46130f8ab43ec3e10d3e5d33fa3f1a1d40f
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-12-21 12:25:36 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -145,7 +145,6 @@ Route::group(['middleware' => ['cors', 'token']], function () {
     });
 
     Route::group(['prefix' => 'wechat'], function () {
-        Route::any('server', 'Api\Wechat\OfficialAccountController@server');
         Route::any('config', 'Api\Wechat\OfficialAccountController@getConfig');
         Route::any('menu/create', 'Api\Wechat\OfficialAccountController@menuCreate');
         Route::any('menu/list', 'Api\Wechat\OfficialAccountController@menuList');
@@ -157,6 +156,7 @@ Route::group(['middleware' => ['cors', 'token']], function () {
 });
 
 Route::group(['prefix' => 'wechat'], function () {
+    Route::any('server', 'Api\Wechat\OfficialAccountController@server');    
     Route::any('oauth', 'Api\Wechat\OfficialAccountController@oauth');
     Route::any('oauth-callback', 'Api\Wechat\OfficialAccountController@oauthCallback');
     Route::any('pay-notify', 'Api\Wechat\PayController@notify');
