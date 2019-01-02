a9b717f3d3716817fbe8d9d681ddb8e031ecd48a
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2019-01-02 18:13:08 +0800

    commit

--- app/Http/Controllers/Api/Coupons/RecordController.php
+++ app/Http/Controllers/Api/Coupons/RecordController.php
@@ -90,6 +90,7 @@ class RecordController extends Controller
     {
         //TODO 判断是否是管理员进行核销
         $admin = Admin::where('fan_id',Token::getUid())->first();
+        dd(\Auth::guard('users')->id());
         if(!isset($admin) || \Auth::guard('users')->id()) {
             return response()->json(['status' => 'error', 'msg' => '你不是管理员，无操作权限']);   
         }
