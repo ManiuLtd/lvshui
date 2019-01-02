02d334887b0aa528814f5b3bf016d414915a4008
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2019-01-02 16:43:20 +0800

    commit

--- app/Http/Controllers/Api/Fans/FanController.php
+++ app/Http/Controllers/Api/Fans/FanController.php
@@ -12,7 +12,7 @@ class FanController extends Controller
     public function fans() {
         $status = request('status');
         $fans = Fan::when($status > -1, function($query) use ($status) {
-            return $query->where('status', $status);
+            return $query->where('subscribe', $status);
         })->paginate(30);
         $today = Fan::whereDate('created_at', date('Y-m-d',time()))->get();
         return response()->json(['status' => 'success','data' => $fans, 'today' => $today]);
