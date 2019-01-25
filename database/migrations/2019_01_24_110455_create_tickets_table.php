6e3cbd576565b02cbb827148246eb298c1ee18fe
AinzOoalGown0420 <1844853576@qq.com>
2019-01-25 23:09:18 +0800

    Update 2019_01_24_110455_create_tickets_table.php

--- database/migrations/2019_01_24_110455_create_tickets_table.php
+++ database/migrations/2019_01_24_110455_create_tickets_table.php
@@ -17,11 +17,12 @@ class CreateTicketsTable extends Migration
             $table->increments('id');
             $table->string('name',100)->comment('名称');
             $table->longText('content')->comment('详细描述');
+            $table->string('remark',300)->commend('备注');
             $table->tinyInteger('total')->comment('商品数量');
             $table->tinyInteger('limit')->comment('用户购买上限');
             $table->tinyInteger('price')->comment('价格');
             $table->tinyInteger('is_up')->default(1)->comment('是否上架,1表是，0表否');
-            $table->dateTime('sratr_date')->comment('开始日期');
+            $table->dateTime('start_date')->comment('开始日期');
             $table->dateTime('end_date')->comment('结束日期');
             $table->timestamps();
         });
