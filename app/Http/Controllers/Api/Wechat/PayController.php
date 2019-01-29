a11e2e42dcecb1672213b842fb1b4f61584cc948
jimocc <245629490@qq.com>
2019-01-29 12:30:01 +0800

    composer

--- app/Http/Controllers/Api/Wechat/PayController.php
+++ app/Http/Controllers/Api/Wechat/PayController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Wechat;
 use App\Models\Activity;
 use App\Models\Fan;
 use App\Models\FanTicket;
+use App\Models\MallGood;
 use App\Models\MallSetting;
 use App\Models\Order;
 use App\Models\OrderSetting;
@@ -43,16 +44,29 @@ class PayController extends Controller
     {
         //TODO: 获取订单信息
         $id = request('id');
-        $order = Order::find($id);
-
+        $order = Order::where('id', $id)->with('orderGoods')->first();
+        $oGoods = $order->orderGoods;
         $result = WechatPay::refund($order);
 
         if($result['result_code'] == 'SUCCESS' && $result['return_msg'] == 'OK') {
-            $sucess = $order->update([ 'use_state'=> -2 ,
-                                       'refund_time'=> date('Y-m-d H:i:s', time()) ]);
-            if($sucess){
+            DB::beginTransaction();
+            try {
+                $order->update([ 'use_state'=> -2 ,
+                                 'refund_time'=> date('Y-m-d H:i:s', time()) ]);
+                if($order->type = Parameter::mall){
+                    Fan::where('id', $order->fan_id)->decrement('point', $order->integral);
+                    foreach ($oGoods as $oGood) {
+                        MallGood::where([['id', $oGood->good_id], ['up_id', $oGood->up_id]])->increment('stock', $oGood->num);
+                    }
+                }else if($order->type = Parameter::ticket){
+
+                }
+                DB::commit();
                 return response()->json(['status' => 'success', 'msg' => '退款成功！']);
+            } catch (\Exception $e) {
+                DB::rollBack();
             }
+
         }else {
             return response()->json(['status' => 'error', 'msg' => $result['err_code_des']]);  
         }  
